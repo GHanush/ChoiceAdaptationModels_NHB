a026121379ca68d7114bd2b0de6bebbaed516c07
anush_ghambaryan <54891480+GHanush@users.noreply.github.com>
2026-02-10 00:14:02 +0100

    Update launch_task.m

--- Exp1/domval/launch_task.m
+++ Exp1/domval/launch_task.m
@@ -1,21 +1,22 @@
 function launch_task
-% Step 1. change to ID of any participant in Experiment 1
+% Step 1. change the value of variable 'isubject' to ID of any participant in Experiment 1
+% Step 2. choose models to run the fitting by commenting out unchosen models
+% Step 3. save changes in steps 1 and 2
+% Step 4. type 'launch_task' in the command window
+
+
 isubject = 131; 
+ima(isubject);
+% dist(isubject);
+% opt(isubject);
+% rl(isubject);
+
 scriptFolder = fileparts(mfilename('fullpath'));
 dataFile = fullfile(scriptFolder, '..', 'data_dom2val.mat');
 load(dataFile);
 tmp = data.subject(isubject);    
 save('tmp.mat','tmp')
 
-% Step 2. choose models to run fitting by commenting out unchosen models
-ima(isubject);
-% dist(isubject);
-% opt(isubject);
-% rl(isubject);
-
-% Step 3. save changes in steps 1 and 2
-% Step 4. type 'launch_task' in the command window
-
-
 end
+
 
