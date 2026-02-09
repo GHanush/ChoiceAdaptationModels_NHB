53c4b728bb4aa67fd59f4a56078f2ed41140fee8
anush_ghambaryan <54891480+GHanush@users.noreply.github.com>
2026-02-09 19:41:59 +0100

    Add files via upload

---
+++ Exp1/domval/launch_task.m
@@ -0,0 +1,19 @@
+function launch_task
+% Step 1. change to ID of any participant in Experiment 1
+isubject = 131; 
+scriptFolder = fileparts(mfilename('fullpath'));
+dataFile = fullfile(scriptFolder, '..', 'data_dom2val.mat');
+load(dataFile);
+tmp = data.subject(isubject);    
+save('tmp.mat','tmp')
+
+% Step 2. choose models to run fitting by commenting out unchosen models
+mix(isubject);
+% dist(isubject);
+% opt(isubject);
+% rl(isubject);
+
+% Step 3. save changes in steps 1 and 2
+% Step 4. type 'launch_task' in the command window
+
+end
