cc8d264b266a400f002827b8fb776a48a22abfbb
anush_ghambaryan <54891480+GHanush@users.noreply.github.com>
2026-02-09 17:42:29 +0100

    Add files via upload

---
+++ Exp1/user_0/launch_task.m
@@ -0,0 +1,15 @@
+function launch_task(isubject)
+scriptFolder = fileparts(mfilename('fullpath'));
+dataFile = fullfile(scriptFolder, '..', 'Exp1', 'data_dom2val.mat');
+load(dataFile);
+tmp = data.subject(isubject);    
+save('tmp.mat','tmp')
+clear
+clc
+
+
+mix(isubject);
+%   dist(isubject);
+%   opt(isubject);
+
+end
