10dcf7b442ea98655209e41863ad7c9a44767cad
anush_ghambaryan <54891480+GHanush@users.noreply.github.com>
2026-02-10 01:17:16 +0100

    Refactor launch_task function structure
    Reorganized the launch_task function by moving the ima call and commented-out model options to the end.

--- Exp1/domval/launch_task.m
+++ Exp1/domval/launch_task.m
@@ -1,23 +1,22 @@
 function launch_task
-% Step 1. change the value of variable 'isubject' to ID of any participant in Experiment 1
-% Step 2. choose models to run the fitting by commenting out unchosen models
-% Step 3. save changes
-% Step 4. type 'launch_task' in the command window
-
 
 isubject = 131; 
-ima(isubject);
-% dist(isubject);
-% opt(isubject);
-% rl(isubject);
-
 scriptFolder = fileparts(mfilename('fullpath'));
 dataFile = fullfile(scriptFolder, '..', 'data_dom2val.mat');
 load(dataFile);
 tmp = data.subject(isubject);    
 save('tmp.mat','tmp')
 
+
+ima(isubject);
+% dist(isubject);
+% opt(isubject);
+% rl(isubject);
+
+
+
 end
+
 
 
 
